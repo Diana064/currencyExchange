5537a913f1cd85bb6409b3e95cef3f9914ca7799
Diana064 <dipustolyakova02@gmail.com>
2023-03-25 00:00:00 +0200

    add curensy exchange

--- src/components/CurrencyRate/CurrencyRate.jsx
+++ src/components/CurrencyRate/CurrencyRate.jsx
@@ -2,16 +2,23 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectBaseCurrency } from 'redux/selectors';
 import { latestCurrency } from 'redux/operation';
+import { selectRates } from 'redux/selectors';
 
 export const CurrencyRate = () => {
   const baseCurrency = useSelector(selectBaseCurrency);
   const dispatch = useDispatch();
+  const ratesCurrency = useSelector(selectRates);
   useEffect(() => {
     dispatch(latestCurrency(baseCurrency));
   }, [baseCurrency, dispatch]);
   return (
     <>
       <p>Ypu base currency : {baseCurrency}</p>
+      <div>
+        {ratesCurrency.map(item => (
+          <p>{item}</p>
+        ))}
+      </div>
     </>
   );
 };
